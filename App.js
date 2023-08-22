1c4f37132c48209a75cb6f4a2fe52770f63ac65a
Osman Herman <osmanherman27@gmail.com>
2023-08-22 20:24:44 -0300

    Desafío entregable N° 1

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hola, Coder!</Text>
+      <Text>Se presenta el Desafío entregable N° 1</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
